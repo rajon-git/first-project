24192a7cf8fa8f4b0565e9d657c14aead259892c
rajon-git <rajon.zhsust15@gmail.com>
2023-03-12 14:39:46 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './component/Footer';
+import Header from './component/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='btn btn-primary'>Hello World</h1>
+      <h2>Bangladesh</h2>
+      <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h1 className='bg-secondary text-white'>Hello from footer</h1>
+        </div>
+    );
+};
+
+export default Footer;
